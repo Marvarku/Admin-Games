54d7a3c4ab2dc0f6ede30f88573a1c99f2ef46dc
Marvelous Arku <marku@itconsortiumgh.com>
2023-03-16 17:05:13 +0000

    Uploaded right file for adding and deletion

--- src/app/dashboards/dashboard1/games-data/games-data.component.ts
+++ src/app/dashboards/dashboard1/games-data/games-data.component.ts
@@ -14,9 +14,24 @@ export class GamesDataComponent implements OnInit {
   rows: any = new Array;
   temp = [...data];
   loadingIndicator = true;
+  isLoading: Boolean = false;
   reorderable = true;
   activeModal: any;
   modalData: any;
+  userId: any;
+  userName: string = '';
+  userEmail : string = '';
+  userAddress: any;
+  status : string = 'Active'
+  users = [
+    { id: '#W83549801', name:'Anna Sthesia', email:'anna@gmail.com', address:'Boston', status:'Active'},
+    { id: '#R67546476', name:'Barb Dwyer', email:'barbdwyer@gmail.com', address:'Washington DC', status:'Inactive'},
+    { id: '#G05654653', name:'Wilma Mumduya', email:'wilmamum@gmail.com', address:'San Francisco', status:'Active'},
+    { id: '#S45645778', name:'Zack Lee', email:'zac@gmail.com',address:'Las Vegas', status:'Inactive'},
+    
+  ]
+
+  
   columns = [{ prop: 'name' }, { name: 'category' }, { name: 'created' }];
   @ViewChild(GamesDataComponent) table: GamesDataComponent | any;
 
@@ -57,8 +72,39 @@ export class GamesDataComponent implements OnInit {
     this.activeModal = this.modalService.open(content, {
         size: modalSize
     });
-
   }
 
+  closeModal() {
+    this.modalService.dismissAll();
+  }
 
+  createUser() {
+    try{
+      this.isLoading = true;
+      const newUser = {
+        id: this.userId,
+        name: this.userName,
+        email:this.userEmail,
+        address:this.userAddress, 
+        status:this.status
+        }
+        this.users.push(newUser);
+        alert('User Added!')
+        this.closeModal();
+    }catch(error){
+      console.log(error);
+      
+    }
+  finally{
+    this.isLoading = false;
+  }
+  }
+
+  
+  deleteUser(user: any) {
+    const index = this.users.findIndex(o => o.id === user.id);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+    }
+  }
 }
